f1bbd88852cd4005d21a452dc35df1d550e3b3ff
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-07-12 20:33:30 +0300

    Modify the phrasing of task One's tests

--- taskOne/stringLength.test.js
+++ taskOne/stringLength.test.js
@@ -4,11 +4,11 @@ test('should have four characters', () => {
     expect(charsLength("abba")).toBe(4);
 })
 
-test('chars > 1', () => {
+test('for chars < 1', () => {
     expect(charsLength("")).toBeUndefined();
 })
 
-test('chars < 10', () => {
+test('for chars > 10', () => {
     expect(charsLength("abcdefghijklmnopqrst")).toBeUndefined();
 })
 
